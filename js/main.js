70db24feaa70d34672c9813c77140112d451916c
saugat shrestha <saugat@skrollx.com>
2019-09-13 17:45:43 +0545

    File Changes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    // Home Slider 
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        dots: false,
+        loop: true,
+        navSpeed: true,
+        smartSpeed: 1000
+    });
+
+});
